454432ecd404f2a82252576200de564ada843fb1
Phil Chen <clw.air@gmail.com>
2016-12-08 13:14:15 +1100

    s

---
+++ D3/src/app.js
@@ -0,0 +1,8 @@
+// npm i -D browser-sync
+// package.json: 
+// "scripts": {
+//    "start": "browser-sync start -s -f index.html src --no-ui --no-notify"
+//  },
+// $ npm start
+
+document.body.innerHTML = `<h1>${d3.version}</h1>`
